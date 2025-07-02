31ae4ee387ce5cab170b12eea82f6fc65c261976
alpha <alpha5611331@gmail.com>
2025-07-02 12:18:06 -0400

    added model fs

--- shared/config/common.py
+++ shared/config/common.py
@@ -3,18 +3,29 @@ from pathlib import Path
 from pydantic_settings import BaseSettings
 
 cur_dir = Path(__file__).parent
+
 root_dir = cur_dir.parent.parent
+
 dataset_dir = root_dir / "dataset"
 dataset_filename = "btc_usdt_tft_dataset.csv"
 dataset_filepath = dataset_dir / dataset_filename
 
+model_dir = root_dir / "model"
+model_filename = "tft"
+model_filepath = model_dir / model_filename
+
 dataset_dir.mkdir(parents=True, exist_ok=True)
+model_dir.mkdir(parents=True, exist_ok=True)
 
 
 class Config(BaseSettings):
     ROOT_DIR: str = str(root_dir)
+
     DATASET_DIR: str = str(dataset_dir)
     DATASET_FILEPATH: str = str(dataset_filepath)
+
+    MODEL_DIR: str = str(model_dir)
+    MODEL_FILEPATH: str = str(model_filepath)
 
 
 config = Config()
