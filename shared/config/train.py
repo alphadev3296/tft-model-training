7b1c6677b90f54b191b97b6d91e4861129d76644
alpha <alpha5611331@gmail.com>
2025-07-02 12:13:15 -0400

    refactor

--- shared/config/train.py
+++ shared/config/train.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Config(BaseSettings):
-    DATASET_SIZE_K: int = 2000  # means 2M dataset
+    DATASET_SIZE: int = 2_000  # means 2M dataset
     DATASET_RESOLUTION_SECS: int = 60  # means 1min
 
 
